fa4e126bf7983d296194dea89e3f9f7102fa5acb
catloc <103197677+catloc@users.noreply.github.com>
2022-04-21 16:37:51 +0300

    v 2.0

--- pingpong.py
+++ pingpong.py
@@ -21,16 +21,16 @@ class Player(GameSprite):
     def update_L(self): #МЕТОД ДЛЯ УПРАВЛЕНИЯ ЛЕВОЙ РАКЕТКОЙ W/S
         keys = key.get_pressed()
         if keys[K_s] and self.rect.x > 5:
-            self.rect.Y -= self.speed
-        if keys[K_w] and self.rect.x < width - 50:
             self.rect.y += self.speed
+        if keys[K_w] and self.rect.x < width - 50:
+            self.rect.y -= self.speed
 
     def update_R(self): #МЕТОД ДЛЯ УПРАВЛЕНИЯ ПРАВОЙ РАКЕТКОЙ UP/DOWN
         keys = key.get_pressed()
         if keys[K_DOWN] and self.rect.x > 5:
-            self.rect.y -= self.speed
-        if keys[K_UP] and self.rect.x < width - 50:
             self.rect.y += self.speed
+        if keys[K_UP] and self.rect.x < width - 50:
+            self.rect.y -= self.speed
 
 width = 700
 height = 500
@@ -39,8 +39,8 @@ display.set_caption("Пинг-Понг")
 back = (200, 255, 255)
 window.fill(back)
 
-speed_x = 3
-speed_y = 3
+speed_x = 2
+speed_y = 2
 
 game = True
 finish = False
@@ -52,9 +52,9 @@ font1 = font.Font(None, 36)
 lose1 = font1.render('Игрок 1 проиграл!', True, (255, 0, 0))
 lose2 = font1.render('Игрок 2 проиграл!', True, (255, 0, 0))
 
-racet1 = Player('ракетки.png', 30, 200, 4, 50, 150)
-racet2 = Player('ракетки.png', 620, 200, 4, 50, 150)
-ball = GameSprite('ball.png', 200, 200, 4, 50, 50)
+racet1 = Player('racet.png', 30, 200, 30, 170, 20)
+racet2 = Player('racet.png', 620, 200, 30, 170, 20)
+ball = GameSprite('ball.png', 200, 200, 50, 50, 30)
 
 while game:
     for e in event.get():
@@ -64,18 +64,30 @@ while game:
         window.fill(back)
         racet1.update_L()
         racet2.update_R()
-        ball.rect.x += speed_x
-        ball.rect.x += speed_x
-
-    if sprite.collide_rect(racet1, ball) or sprite.collide_rect(racet2, ball):
-        speed_x *= -1
-
-    if ball.rect.y > width - 50 or ball.rect.x < 0:
-        speed_y *= -1
-
+        ball.update()
         racet1.reset()
         racet2.reset()
         ball.reset()
 
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+    if sprite.collide_rect(racet1, ball) or sprite.collide_rect(racet2, ball):
+        speed_x *= -1
+
+    if ball.rect.y > height-5 or ball.rect.y < 0:
+        speed_y *= -1
+
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1, (200, 200))
+
+    if ball.rect.x > 625:
+        finish = True
+        window.blit(lose2, (200, 200))
+
+        
+
+       
     display.update()
     clock.tick(FPS)
